7eea29a8ffbb74c5687b5f92f7b8f967fe2a729a
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2026-04-27 19:16:42 +0530

    new fix

--- api/index.py
+++ api/index.py
@@ -1,12 +1,1 @@
-import traceback
-try:
-    from app.main import app
-except Exception as e:
-    from fastapi import FastAPI
-    from fastapi.responses import PlainTextResponse
-    
-    app = FastAPI()
-    
-    @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
-    def catch_all(path: str):
-        return PlainTextResponse("Hello from fallback! Error: " + traceback.format_exc(), status_code=200)
+from app.main import app
